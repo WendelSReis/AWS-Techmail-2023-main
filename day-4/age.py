ac7323aea919f7be909d9d8aa431d1fc876e1d8b
Wendel Reis <wendel.m.w18@gmail.com>
2023-05-12 17:29:26 -0300

    dev-day-4

---
+++ day-4/age.py
@@ -0,0 +1,19 @@
+def printCategory(age):
+    if age > 18:
+        print('Adult')
+    elif age > 65:
+        print('Senior Citizen')
+    else:
+        print('Child')
+        
+printCategory(70)
+printCategory(10)
+
+"""
+Adult
+"""
+
+"""
+Adult
+Child
+"""
